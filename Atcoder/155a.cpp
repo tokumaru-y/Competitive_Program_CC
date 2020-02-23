09e984de2183331b0988ae223a1d553f97d2c1d1
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-02-23 23:17:54 +0900

    6

--- Atcoder/155a.cpp
+++ Atcoder/155a.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main () {
     int a,b,c;
-    cin >> a >> b >> c;
-    
+    scanf("%d%d%d", &a, &b, &c);
+    int ispoor = 0;
+    if (a==b && b != c) ispoor = 1;
+    if (b==c && c != a) ispoor = 1;
+    if (c==a && a != b) ispoor = 1;
+    if (ispoor) {
+        puts("Yes");
+    } else {
+        puts("No");
+    }
 }
