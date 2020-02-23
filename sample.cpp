dc7aed1b86e519dca52ba79dd3f41f469416569d
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-02-23 20:28:53 +0900

    3

--- sample.cpp
+++ sample.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-  cout << "こんにちは" << endl;
-  cout << "AtCoder" << endl;
+  for (int i = 1; i < 10; ++i) {
+    for (int j = 1; j < 10; ++j) {
+      cout << i * j << "\t";
+    }
+  }
 }
