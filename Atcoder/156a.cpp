50f55fef36b33a71cf491fccb6f6e166fa8aa026
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-02-23 19:30:50 +0900

    20200223 2

---
+++ Atcoder/156a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,r;
+    cin >> n >> r;
+    if (n >= 10) {
+        cout << r << endl;
+    } else {
+        cout << r + (100*(10-n)) << endl;
+    }
+}
