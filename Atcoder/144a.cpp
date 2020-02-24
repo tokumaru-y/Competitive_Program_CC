961d8e4b4fd49b393d604b42e2de5f1406dba496
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-02-24 11:30:03 +0900

    20200224

---
+++ Atcoder/144a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a >= 10 || b >= 10){
+        cout << -1 << endl; 
+    } else {
+        cout << a * b << endl;
+    }
+}
