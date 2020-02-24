4d17fd4594f11c1f6ffe9c103d3599a665b724d8
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-02-24 13:41:28 +0900

    8

---
+++ Atcoder/130a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a < b) {
+        cout << 0 << endl;
+    } else {
+        cout << 10 << endl;
+    }
+}
