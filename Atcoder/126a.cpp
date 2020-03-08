2fd2659fd68b231711e8b1ad66eedce9ca098a79
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-08 18:39:15 +0900

    2

--- Atcoder/126a.cpp
+++ Atcoder/126a.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 int main() {
-    int b,c;
-    scanf("%d%d",&b,&c);
-    char a[b];
-    scanf("%c%*c",a);
-    a[c-1] += 'a' - 'A';
-    printf("%s", "aaaaaa");
-    printf("%c", *a);
-}
+    int n,k;
+    scanf("%d %d", &n,&k);
+    char s[n];
+    scanf("%s", s);
+    for(int i = 0; i <n;i++){
+        
+        if(i == k-1) {
+            printf("%c", s[i]+32);
+        }else{
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+} 
