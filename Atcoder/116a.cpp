8eaaffad4381828c3927cea7733a6145806ea8e7
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-08 23:49:46 +0900

    7

---
+++ Atcoder/116a.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+using namespace std;
+
+int main () {
+    int a,b,c;
+    scanf("%d %d %d", &a,&b,&c);
+    int ans = a * b / 2 ;
+    printf("%d",ans);
+}
---
+++ Atcoder/117a.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    float t,x;
+    scanf("%f %f", &t,&x);
+    float ans;
+    ans = t/x;
+    printf("%f", ans);
+}
