e68856b3976f871fa518415ef8b7b53dd646c711
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-14 01:12:56 +0900

    8

---
+++ Atcoder/100a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a,&b);
+    if(a > 8 || b >8) {
+        printf("%s", ":(");
+    } else {
+        printf("%s", "Yay!");
+    }
+}
