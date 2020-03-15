a12ff32e7203036a824640b8001109358609516b
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-16 00:48:48 +0900

    7

---
+++ Atcoder/058a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(b-a==c-b){
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+}
