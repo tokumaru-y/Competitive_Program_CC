683169f1544a903e5e69bc0ebf786b2419dd3141
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-15 23:19:54 +0900

    solve D

--- Educational DP/D.cpp
+++ Educational DP/D.cpp
@@ -3,18 +3,22 @@ using namespace std;
 int main () {
     int n,w;
     scanf("%d %d",&n,&w);
-    long long dp[n][w] ={0};
-    long long ll[n][2];
+    long long dp[n+1][w+1] ={};
+
+    long long ll[n+1][2]={};
     for(int i=0;i<n;i++){
-        int a,b;
-        scanf("%d %d",&a,&b);
+        long long  a,b;
+        scanf("%lld %lld",&a,&b);
         ll[i][0]=a;ll[i][1]=b;
     }
     for(int i=0;i<n;i++){
-        for(int j=0;j<w;j++){
-            if(j-ll[n][0]>=0){
-                dp[]
+        for(int j=0;j<=w;j++){
+            if(j-ll[i][0]>=0){
+                dp[i+1][j] > dp[i][j-ll[i][0]] + ll[i][1] ? dp[i+1][j] = dp[i+1][j] : dp[i+1][j]= dp[i][j-ll[i][0]] + ll[i][1];
             }
+            dp[i+1][j] > dp[i][j] ? dp[i+1][j] = dp[i+1][j] : dp[i+1][j] = dp[i][j];
         }
     }
+
+    printf("%lld", dp[n][w]);
 }
