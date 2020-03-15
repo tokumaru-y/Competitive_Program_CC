5f313ba22af25d3f5179c789e8e61a93346aa2f7
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-16 00:15:31 +0900

    2

---
+++ Atcoder/063a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if (a+b>=10){
+        printf("%s", "error");
+    } else {
+        printf("%d",a+b);
+    }
+}
