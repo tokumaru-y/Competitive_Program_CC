787e31a42fbb2bf177ae6df3244023d984680cf4
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-15 13:03:39 +0900

    5

---
+++ Atcoder/070a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    char s[3];
+    scanf("%s",s);
+    if(s[0] == s[2]){
+        printf("%s","Yes");
+    } else {
+        printf("%s", "No");
+    }
+}
