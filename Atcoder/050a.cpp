2ef35e845d7d11dfcf6c1cae25a3aed02a38f644
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-17 00:46:32 +0900

    5

---
+++ Atcoder/050a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a,b;
+    char x;
+    scanf("%d %c %d",&a,&x,&b);
+    if(x=='+'){
+        printf("%d", a+b);
+    } else {
+        printf("%d", a-b);
+    }
+}
