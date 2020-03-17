460798215dad8b5613b516678e3321c9a2104b75
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-18 01:06:06 +0900

    0318

---
+++ Atcoder/038a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    string s;
+    cin >> s;
+    if(s[s.length()-1] == 'T'){
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+}
