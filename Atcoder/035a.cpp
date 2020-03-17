42f10e437d8f16676c75c89cdcad6ce863ab7c5a
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-18 01:14:27 +0900

    0318

---
+++ Atcoder/035a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a,b;scanf("%d %d",&a,&b);
+    if(a*3==b*4){
+        printf("4:3\n");
+    } else {
+        printf("16:9\n");
+    }
+}
