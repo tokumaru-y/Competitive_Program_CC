98f54651180c9c30a172efebf84968dfd33ea1a8
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-18 00:57:09 +0900

    0318

---
+++ Atcoder/040a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a-1>a-b){
+        printf("%d",a-b);
+    } else {
+        printf("%d",a-1);
+    }
+}
