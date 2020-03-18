95deca1eccd70342c4e03910d4ff247a31683272
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-19 01:00:53 +0900

    0319

---
+++ Atcoder/034a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a<b){
+        printf("Better");
+    } else {
+        printf("Worse");
+    }
+}
