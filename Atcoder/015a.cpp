f33ec9bd16c800ae43ce5655b8593de864e8fd9b
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-20 01:01:23 +0900

    0320

---
+++ Atcoder/015a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    string a,b;
+    cin >> a;cin >> b;
+    if(a.length()>b.length()){
+        cout << a << endl;
+    } else {
+        cout << b << endl;
+    }
+}
