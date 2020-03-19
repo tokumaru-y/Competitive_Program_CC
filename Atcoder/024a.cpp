b928a012a2b76608384ae6f844184c980bbc148c
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-19 23:37:54 +0900

    0320

---
+++ Atcoder/024a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a,b,c,k,s,t;
+    scanf("%d %d %d %d",&a,&b,&c,&k);
+    scanf("%d %d",&s,&t);
+    int ans = a*s+b*t;
+    if(s+t>=k){
+        ans -= (s+t)*c;
+    }
+    printf("%d",ans);
+}
