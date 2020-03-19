d31c23109a776c644ba33d58806ebb08cd4644cb
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-03-20 01:59:38 +0900

    0320

--- Atcoder/158a.cpp
+++ Atcoder/158a.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main () {
     char s[3];
     scanf("%s",s);
-    if(s[0] == s[1] || s[0] == s[2] || s[1] == s[2]){
-        printf("No");
-    } else {
+    if(s[0] != s[1] || s[1] != s[2]){
         printf("Yes");
+    } else {
+        printf("No");
     }
 }
