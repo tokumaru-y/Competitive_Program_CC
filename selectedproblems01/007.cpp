f3d916676682b5cc13c8dafe06a7559a4cc4cce3
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-04-10 02:31:03 +0900

    04010

--- selectedproblems01/007.cpp
+++ selectedproblems01/007.cpp
@@ -21,16 +21,27 @@ vector<long long> divisor(long long n) {
 }
 signed main () {
     int n;scanf("%d",&n);
-    vector<vector<bool>> list(3001,vector<bool>(3001,false));
+    vector<vector<bool>> list(5001,vector<bool>(5001,false));
+    vector<vector<int>> originlist(n, vector<int>(2,0));
     REP(i,n){
         int x,y;scanf("%d %d",&x,&y);
-        list[x-1][y-1] = true;
+        list[x][y] = true;
+        originlist[i][0]=x;originlist[i][1]=y;
     }
     int ans=0;
-    REP(i,3001){
-        REP(j,3001){
-            if(i==j)continue;
-            int distance = 
+    for(int i = 0;i<n;i++){
+        for(int j = i+1;j<n;j++){
+            int x1 = originlist[i][0]; int y1 = originlist[i][1];
+            int x2 = originlist[j][0]; int y2 = originlist[j][1];
+            int tmp1 = x2-y2+y1;int tmp2 = y2+x2-x1;int tmp3 = x1-y2+y1; int tmp4 = y1+x2-x1;
+            if (tmp1 < 0 || tmp1 > 5000 ||
+                tmp2 < 0 || tmp2 > 5000 ||
+                tmp3 < 0 || tmp3 > 5000 ||
+                tmp4 < 0 || tmp4 > 5000 ) continue;
+            if (list[tmp1][tmp2] == true && list[tmp3][tmp4] == true){
+                ans = max(ans, (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+            }
         }
     }
+    printf("%d\n",ans);
 }
