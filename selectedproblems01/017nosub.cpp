0cdba869cbdcead809e71eb977c384e19ea2f46a
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-04-13 20:04:49 +0900

    0412

---
+++ selectedproblems01/017nosub.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i= (a); i<(b); i++)
+#define REP(i,n) for(int i=0;i < (n);i++)
+#define ALL(obj) (obj).begin(), (obj).end()
+using namespace std;
+
+const long long INF = 1LL << 60;
+const int MOD = (int)1e9 + 7;
+typedef long long ll;
+//約数列挙
+vector<long long> divisor(long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    //sort(ret.begin(), ret.end()); // 昇順に並べる
+    return ret;
+}
+signed main () {
+    
+}
