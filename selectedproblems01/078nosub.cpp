68ff34f06b14653c18b10cb2933f67be2e5a2ba2
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-04-28 10:22:24 +0900

    0428

--- selectedproblems01/078nosub.cpp
+++ selectedproblems01/078nosub.cpp
@@ -22,7 +22,7 @@ vector<long long> divisor(long long n) {
     return ret;
 }
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
-
+/*
 signed main () {
     int h,w;scanf("%d %d",&h,&w);
     SCANF(k);
@@ -57,5 +57,37 @@ signed main () {
         int ocean=oc[x2][y2] - oc[x1][y2] - oc[x2][y1] + oc[x1][y1];;
         int ice=ic[x2][y2] - ic[x1][y2] - ic[x2][y1] + ic[x1][y1];;
         printf("%d %d %d\n",jungle,ocean,ice);
+    }
+}*/
+signed main(){
+    int h,w;scanf("%d %d",&h,&w);
+    int k;scanf("%d",&k);
+    vector<vector<int>> jun(h+1,vector<int>(w+1,0));
+    vector<vector<int>> oce(h+1,vector<int>(w+1,0));
+    vector<vector<int>> ice(h+1,vector<int>(w+1,0));
+    REP(a,h){
+        REP(b,w){
+            char tmp;cin >> tmp;
+            if(tmp=='J')jun[a+1][b+1]++;
+            if(tmp=='O')oce[a+1][b+1]++;
+            if(tmp=='I')ice[a+1][b+1]++;
+        }
+    }
+    vector<vector<int>> j(h+1,vector<int>(w+1,0));
+    vector<vector<int>> o(h+1,vector<int>(w+1,0));
+    vector<vector<int>> i(h+1,vector<int>(w+1,0));
+    REP(a,h){
+        REP(b,w){
+            j[a+1][b+1]=j[a+1][b]+j[a][b+1]-j[a][b]+jun[a+1][b+1];
+            o[a+1][b+1]=o[a+1][b]+o[a][b+1]-o[a][b]+oce[a+1][b+1];
+            i[a+1][b+1]=i[a+1][b]+i[a][b+1]-i[a][b]+ice[a+1][b+1];
+        }
+    }
+    REP(a,k){
+        int x1,y1,x2,y2;scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+        int jungle = j[x2][y2] - j[x2][y1-1] - j[x1-1][y2] + j[x1-1][y1-1];
+        int ocean = o[x2][y2] - o[x2][y1-1] - o[x1-1][y2] + o[x1-1][y1-1];
+        int ices = i[x2][y2] - i[x2][y1-1] - i[x1-1][y2] + i[x1-1][y1-1];
+        cout << jungle << ' ' << ocean << ' ' << ices << endl;
     }
 }
