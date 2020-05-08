1871a99e9c85faaf112403553cd548ad8e9695d3
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-05-08 18:02:23 +0900

    0506

--- Ant Book Practice/firstclass/2/ABC038DNOSUB.cpp
+++ Ant Book Practice/firstclass/2/ABC038DNOSUB.cpp
@@ -3,12 +3,12 @@
 #define REP(i,n) for(int i=0;i < (n);i++)
 #define ALL(obj) (obj).begin(),(obj).end()
 #define SCANF(i) int i;scanf("%d",&i)
-typedef long long ll;
 using namespace std;
-using P = pair<int,int>;
 const long long INF = 1LL << 60;
 const int IINF=100000000;
 const int MOD = (int)1e9 + 7;
+typedef long long ll;
+using P = pair<ll,ll>;
 //約数列挙
 vector<long long> divisor(long long n) {
     vector<long long> ret;
@@ -22,7 +22,13 @@ vector<long long> divisor(long long n) {
     return ret;
 }
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
-
+vector<P> containers;
 signed main () {
-
+    int n;cin >> n;
+    REP(i,n){
+        ll w,h;cin >> w >> h;
+        containers.push_back(P(w,h));
+    }
+    sort(ALL(containers),greater<P>());
+    
 }
