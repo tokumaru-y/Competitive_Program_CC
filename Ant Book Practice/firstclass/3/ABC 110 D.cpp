274aea1e3a7c59922b0c892d179a85456ab5b335
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-05-19 02:09:01 +0900

    0519

---
+++ Ant Book Practice/firstclass/3/ABC 110 D.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) for(int i=0;i < (n);i++)
+#define ALL(obj) (obj).begin(),(obj).end()
+#define SCANF(i) int i;scanf("%d",&i)
+typedef long long ll;
+using namespace std;
+using P = pair<int,int>;
+const long long INF = 1LL << 60;
+const int IINF=100000000;
+const int MOD = (int)1e9 + 7;
+//約数列挙
+vector<long long> divisor(long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    //sort(ret.begin(), ret.end()); // 昇順に並べる
+    return ret;
+}
+vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
+vector<pair<long long, long long> > prime_factorize(long long n) {
+    vector<pair<long long, long long> > res;
+    for (long long p = 2; p * p <= n; ++p) {
+        if (n % p != 0) continue;
+        int num = 0;
+        while (n % p == 0) { ++num; n /= p; }
+        res.push_back(make_pair(p, num));
+    }
+    if (n != 1) res.push_back(make_pair(n, 1));
+    return res;
+}
+
+const int MAX = 210000;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+void COMinit(){
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for(int i = 2; i < MAX; i++){
+        fac[i] = fac[i-1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD/i) % MOD;
+        finv[i] = finv[i-1] * inv[i] % MOD;
+    }
+}
+long long COM(int n, int k){
+    if(n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD;
+}
+signed main () {
+    ll n,m;cin >> n >> m;
+    COMinit();
+    auto vecto = prime_factorize(m);
+    ll res = 1;
+    for(auto pai : vecto){
+        ll num = pai.second;
+        ll tmp = COM(num+n-1, n-1);
+        res*=tmp;res%=MOD;
+    }
+    cout << res << endl;
+}
