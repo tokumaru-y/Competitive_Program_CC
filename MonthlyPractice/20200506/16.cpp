8e47d12de4225208dcbaf598e2d791e40741f97d
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-05-28 04:02:21 +0900

    20200528

---
+++ MonthlyPractice/20200506/16.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) for(int i=0;i < (n);i++)
+#define ALL(obj) (obj).begin(),(obj).end()
+#define SCANF(i) int i;scanf("%d",&i)
+typedef long long ll;
+using namespace std;
+using P = pair<int,int>;
+const long long INF = 1LL << 60;
+const int IINF=100000000;
+const int MOD = (int)1e9 + 7;
+//約数列挙
+vector<long long> divisor(long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    //sort(ret.begin(), ret.end()); // 昇順に並べる
+    return ret;
+}
+vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
+
+signed main () {
+    ll n;cin >> n;
+    ll ans=0;
+    vector<ll> divlist = divisor(n);
+    for(ll d : divlist){
+        ll m = (n/d)-1;
+        if(n==m*d + d && d < m)ans+=m;
+    }
+    cout << ans << endl;
+}//https://atcoder.jp/contests/diverta2019/tasks/diverta2019_d
