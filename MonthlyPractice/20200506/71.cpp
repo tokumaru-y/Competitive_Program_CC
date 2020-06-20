c52769929c6a999d4c4c363e095c87d0df4e980b
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-06-21 00:55:53 +0900

    20200620

---
+++ MonthlyPractice/20200506/71.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) for(int i=0;i < (n);i++)
+#define ALL(obj) (obj).begin(),(obj).end()
+#define SCANF(i) int i;scanf("%d",&i)
+typedef long long ll;
+using namespace std;
+using P = pair<int,int>;
+const long long INF = 1LL << 60;
+const int IINF=100000000;
+const int MOD = (int)1e9 + 7;
+//約数列挙
+vector<long long> divisor(long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    //sort(ret.begin(), ret.end()); // 昇順に並べる
+    return ret;
+}
+vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
+
+signed main () {
+    int n;cin >> n;
+    ll maans = 0;
+    ll mians;
+    vector<ll> inlist(n);
+    REP(i,n)cin >> inlist[i];
+    sort(ALL(inlist),greater<ll>());
+    REP(i,n)maans+=inlist[i];
+    mians=inlist[0];
+    FOR(i,1,n){
+        mians-=inlist[i];
+    }
+    mians=max(mians,(ll)0);
+    cout << maans << endl;
+    cout << mians << endl;
+}//https://atcoder.jp/contests/arc004/tasks/arc004_2
