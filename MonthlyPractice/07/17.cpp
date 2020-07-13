4e9c562de75f89789da72446e5b22ffe7fd97322
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-07-13 22:04:54 +0900

    0713

--- MonthlyPractice/07/17.cpp
+++ MonthlyPractice/07/17.cpp
@@ -24,4 +24,80 @@ vector<long long> divisor(long long n) {
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
-}
+    ll n,k;cin >> n >> k;
+    vector<ll> pl;vector<ll> min;
+    REP(i,n){
+        ll tmp;cin >> tmp;
+        if(tmp<0)min.push_back(tmp);
+        else pl.push_back(tmp);
+    }
+    ll pllen=pl.size();ll minlen=min.size();
+    sort(ALL(pl),greater<ll>());
+    sort(ALL(min));
+    ll ans=1;
+    bool is_ans_plus=false;
+    if(pllen==0){
+        if(k%2==0)is_ans_plus=true;
+    } else {
+        if(n>k || (n==k && minlen%2==0))is_ans_plus=true;
+    }
+    if(is_ans_plus){
+        ll pindex=0;
+        ll mindex=0;
+        if(pllen>0 && k%2==1){
+            pindex=1;
+            ans*=pl[0];
+            ans%=MOD;
+            k--;
+        }
+        while(k>0){
+            bool canp=(pindex<pllen-1)?true:false;
+            bool canm=(mindex<minlen-1)?true:false;
+            if(k==1){
+                ans*=pl[pindex];
+                ans%=MOD;
+                pindex++;
+                k--;
+            } else if(canp && canm){
+                ll t1=pl[pindex]*pl[pindex+1];
+                ll t2=min[mindex]*min[mindex+1];
+                if(t1>t2){
+                    t1%=MOD;
+                    ans*=t1;
+                    ans%=MOD;
+                    pindex+=2;
+                    k-=2;
+                } else {
+                    t2%=MOD;
+                    ans*=t2;
+                    ans%=MOD;
+                    mindex+=2;
+                    k-=2;
+                }
+            } else if(canp){
+                ll t1=pl[pindex]*pl[pindex+1];
+                t1%=MOD;
+                ans*=t1;
+                ans%=MOD;
+                pindex+=2;
+                k-=2;
+            } else {
+                ll t2=min[mindex]*min[mindex+1];
+                t2%=MOD;
+                ans*=t2;
+                ans%=MOD;
+                mindex+=2;
+                k-=2;
+            }
+        }
+    } else {
+        vector<ll> abslist;
+        REP(i,pllen)abslist.push_back(pl[i]);
+        REP(i,minlen)abslist.push_back(abs(min[i]));
+        sort(ALL(abslist));
+        REP(i,k)ans*=abslist[i],ans%=MOD;
+        ans*=-1;
+        ans+=MOD;
+    }
+    cout << ans << endl;
+}//https://atcoder.jp/contests/abc173/tasks/abc173_e
