ee237d9aa472f97962e6110ac1c762624f98adf8
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-08-30 13:25:33 +0900

    3

--- MonthlyPractice/8/12.cpp
+++ MonthlyPractice/8/12.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#define FOR(i,a,b) for(int i=(a);i<(b);i++)
-#define REP(i,n) for(int i=0;i < (n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<(b);i++)
+#define REP(i,n) for(ll i=0;i < (n);i++)
 #define ALL(obj) (obj).begin(),(obj).end()
 #define SCANF(i) int i;scanf("%d",&i)
 typedef long long ll;
@@ -24,4 +24,20 @@ vector<long long> divisor(long long n) {
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
-}
+    ll n;cin >> n;
+    vector<ll> tako(n);
+    REP(i,n)cin >> tako[i];
+    ll ans = 0;
+    ll sum = 0;
+    REP(i,n){
+        sum += tako[i];
+        sum %= MOD;
+    }
+    REP(i,n-1){
+        sum -= tako[i];
+        if(sum < 0)sum+=MOD; // ここがないとうまく回らない。
+        ans += (sum* tako[i])%MOD;
+        ans %= MOD;
+    }
+    cout << ans << endl;
+}//https://atcoder.jp/contests/abc177/tasks/abc177_c
