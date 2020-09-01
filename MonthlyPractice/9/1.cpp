eafadedb325d66e8d09eeb88bcc11047bca52314
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-01 22:47:50 +0900

    1

---
+++ MonthlyPractice/9/1.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) for(int i=0;i < (n);i++)
+#define ALL(obj) (obj).begin(),(obj).end()
+#define SCANF(i) int i;scanf("%d",&i)
+typedef long long ll;
+using namespace std;
+using P = pair<int,int>;
+const long long INF = 1LL << 60;
+const int IINF=100000000;
+const int MOD = (int)1e9 + 7;
+//約数列挙
+vector<long long> divisor(long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    //sort(ret.begin(), ret.end()); // 昇順に並べる
+    return ret;
+}
+vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
+
+signed main () {
+    int a,b,c;cin >> a >> b >> c;
+    int ans = 0;int tmp = 0;
+    while(tmp<a){
+        tmp+=b;
+        ans+=c;
+    }
+    cout << ans << endl;
+}//https://atcoder.jp/contests/abc176/tasks/abc176_a
