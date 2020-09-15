74381c0da991786984895ad3848d50cc69a02f5a
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-15 22:17:05 +0900

    1

---
+++ MonthlyPractice/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/81906/competitive_program_c++/MonthlyPractice/hello/target/debug/hello: /mnt/c/Users/81906/competitive_program_c++/MonthlyPractice/hello/src/main.rs
--- template.py
+++
@@ -1,6 +0,0 @@
-
-def solve():
-    
-
-if __name__ == '__main__':
-    solve()
