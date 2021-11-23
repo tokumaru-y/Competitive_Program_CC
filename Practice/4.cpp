74c8f428f9e75842931f0d0fde04e8121564c565
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 16:02:56 +0900

    4

--- Practice/4.cpp
+++ Practice/4.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/arc093/tasks/arc093_a
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,18 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N;
+    cin >> N;
+    vector<int> A(N+2, 0);
+    ll sum = 0;
+    REP(i,N){
+        cin >> A[i+1];
+        sum += abs(A[i] - A[i+1]);
+    }
+    sum += abs(A[N]);
+    for (int i=1;i<=N;i++){
+        int dif = abs(A[i-1] - A[i]) + abs(A[i+1] - A[i]);
+        int plus = abs(A[i-1] - A[i+1]);
+        cout << sum - dif + plus << endl;
+    }
 }
