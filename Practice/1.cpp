30c1fdfb22216ebce63c444cf708db9ef84f9921
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 11:04:36 +0900

    1

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,7 @@
+// https://atcoder.jp/contests/abc029/tasks/abc029_c
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
-#define REP(i,n) for(int i=0;i < (n);i++)
+#define REP(i,n) for(int i=0;i < int(n);i++)
 #define ALL(obj) (obj).begin(),(obj).end()
 #define SCANF(i) int i;scanf("%d",&i)
 typedef long long ll;
@@ -11,5 +12,27 @@ const int IINF=100000000;
 const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
+void dfs(vector<string> &ans) {
+    signed int s = ans.size();
+    vector<string> tmp;
+    for (char x = 'a'; x<='c';x++){
+        for(string y: ans) {
+            tmp.push_back(y+x);
+        }
+    }
+    ans = tmp;
+}
+
 signed main () {
+    int N;
+    cin >> N;
+    vector<string> ans = {"a", "b", "c"};
+    REP(i,N-1){
+        dfs(ans);
+    }
+    sort(ans.begin(), ans.end());
+    for (string a:ans){
+        cout << a << endl;
+    }
+    return 0;
 }
