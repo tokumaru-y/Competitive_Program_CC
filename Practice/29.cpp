42bff622e05b95ba51e89aa47ba0ba871d65266b
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 23:02:07 +0900

    make files

---
+++ Practice/29.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) for(int i=0;i < (n);i++)
+#define ALL(obj) (obj).begin(),(obj).end()
+#define SCANF(i) int i;scanf("%d",&i)
+typedef long long ll;
+using namespace std;
+using P = pair<int,int>;
+const long long INF = 1LL << 60;
+const int IINF=100000000;
+const int MOD = (int)1e9 + 7;
+vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
+
+signed main () {
+}
