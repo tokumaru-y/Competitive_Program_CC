76873610a45bbafe49cdba9943c8d51cb66d7ffe
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 11:50:57 +0900

    3

--- Practice/3.cpp
+++ Practice/3.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/arc099/tasks/arc099_a
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,14 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N,K;
+    cin >> N >> K;
+    vector<int> A(N,-1);
+    REP(i,N)cin >> A[i];
+    sort(ALL(A));
+    A.erase(unique(ALL(A)), A.end());
+    int s = (int)A.size() - 1;
+    int ans = s / (K-1);
+    if (s%(K-1) > 0)ans++;
+    cout << ans << endl;
 }
