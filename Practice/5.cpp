913eeb4ae610ff3b714b5abd8235fe822192688a
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 16:08:41 +0900

    5

--- Practice/5.cpp
+++ Practice/5.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/code-festival-2014-final/tasks/code_festival_final_d
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,7 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    ll N;
+    cin >> N;
+    cout << N+1 << " " << 2 << endl;
 }
