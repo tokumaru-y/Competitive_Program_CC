560336d4c51d6f18ce3848523fad811119dac972
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 22:37:16 +0900

    8

--- Practice/8.cpp
+++ Practice/8.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/joi2008ho/tasks/joi2008ho_c
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,29 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N,M;
+    cin >> N >> M;
+    vector<int> A(N,-1);
+    REP(i,N)cin >> A[i];
+    vector<ll> sum_list;
+    REP(i,N){
+        sum_list.push_back(A[i]);
+        REP(j,N){
+            ll tmp = A[i]+A[j];
+            sum_list.push_back(tmp);
+        }
+    }
+    sort(sum_list.begin(), sum_list.end());
+    ll ans = 0;
+    A.push_back(0);
+    REP(i,N+1)REP(j,N+1){
+        ll tmp = A[i]+A[j];
+        ll num_left = M - tmp;
+        if (num_left<0)continue;
+        auto itr = lower_bound(sum_list.begin(), sum_list.end(),num_left);
+        if (*itr > num_left && itr == sum_list.begin())continue;
+        if (*itr > num_left)itr--;
+        ans = max(ans, *itr+tmp);
+    }
+    cout << ans << endl;
 }
