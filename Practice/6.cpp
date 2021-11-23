e82754415a2a4d4dc43ca2716cd0cd08ccb2dd5f
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 16:43:15 +0900

    5

--- Practice/6.cpp
+++ Practice/6.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc040/tasks/abc040_c
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,16 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N;
+    cin >> N;
+    vector<ll> A(N,INF);
+    REP(i,N)cin >> A[i];
+    vector<ll> dp(N+1, INF);
+    dp[0] = 0;
+    dp[1] = 0;
+    for(int i=1;i<N;i++){
+        dp[i+1] = min(dp[i] + abs(A[i] - A[i-1]), dp[i+1]);
+        if (i-2>=0)dp[i+1] = min(dp[i-1] + abs(A[i] - A[i-2]), dp[i+1]);
+    }
+    cout << dp[N] << endl;
 }
