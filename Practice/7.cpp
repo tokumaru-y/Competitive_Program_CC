93f38c35fa37e03a9e71fb3757bc4a8b4229579c
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-23 17:55:24 +0900

    7

--- Practice/7.cpp
+++ Practice/7.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/code-festival-2017-quala/tasks/code_festival_2017_quala_c
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,38 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int H,W;
+    cin >> H >> W;
+    vector<vector<char>> A(H, vector<char>(W));
+    priority_queue<int>que;
+    unordered_map<char, int>mp;
+    REP(i,H)REP(j,W){
+        cin >> A[i][j];
+        mp[A[i][j]]++;
+    }
+    if (H==1 && W==1){
+        cout << "Yes" << endl;
+        return 0;
+    }
+    for (auto itr=mp.begin();itr!=mp.end();itr++){
+        que.push(itr->second);
+    }
+    for(int w=0;w<(W/2);w++){
+        if (que.size()==0){
+            cout << "No" << endl;
+            return 0;
+        }
+        while (que.top() < 4){
+            que.pop();
+            if (que.size()==0){
+                cout << "No" << endl;
+                return 0;
+            }
+        }
+        int top = que.top();
+        que.pop();
+        que.push(top-4);
+    }
+    if(W%2==1){}
+
 }
