fcf4b30dc28871657d836cedac2a2110b1d90296
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-11-29 23:13:51 +0900

    13

--- Practice/13.cpp
+++ Practice/13.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/arc013/tasks/arc013_1
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,16 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    vector<int> X(3,-1);
+    vector<int> Y(3,-1);
+    REP(i,3)cin >> X[i];
+    REP(i,3)cin >> Y[i];
+    sort(ALL(Y));
+    int ans = 0;
+    do {
+        int cnt = 1;
+        REP(i,3)cnt *= X[i] / Y[i];
+        ans = max(ans, cnt);
+    } while(next_permutation(Y.begin(), Y.end()));
+    cout << ans << endl; 
 }
