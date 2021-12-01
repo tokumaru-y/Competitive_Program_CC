cc6210eec0999ad186b423ff9ee1a1c3f22a8c8a
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-12-01 22:30:41 +0900

    14

--- Practice/14.cpp
+++ Practice/14.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/arc130/tasks/arc130_b
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,25 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int H,W,C,Q;
+    cin >> H >> W >> C >> Q;
+    vector<vector<int>> TNC(Q, vector<int>{-1,-1,-1});
+    REP(i,Q)cin >> TNC[i][0] >> TNC[i][1] >> TNC[i][2];
+    reverse(ALL(TNC));
+    set<int>used_h;set<int>used_w;
+    vector<ll> ans(C+1,0);
+    REP(i,Q){
+        int t = TNC[i][0];int n = TNC[i][1];int c = TNC[i][2];
+        if (t==1 && used_h.find(n) == used_h.end()){
+            used_h.insert(n);
+            ans[c] += W - used_w.size();
+        } else if (t==2 && used_w.find(n) == used_w.end()){
+            used_w.insert(n);
+            ans[c] += H - used_h.size();
+        }
+    }
+    REP(i,C){
+        cout << ans[i+1] << " ";
+    }
+    cout << endl;
 }
