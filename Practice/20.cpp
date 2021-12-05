5ffe1e3e038c5dad2e641dfa6a6dd82728dc8b0e
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-12-05 17:17:44 +0900

    20

--- Practice/20.cpp
+++ Practice/20.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc079/tasks/abc079_c
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,28 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    string a;cin >> a;
+    for(int bit=0;bit<(1<<3);bit++){
+        int pro_num = a[0] - '0';
+        REP(j,3){
+            if (bit & (1<<j)){
+                pro_num -= a[j+1] - '0';
+            } else {
+                pro_num += a[j+1] - '0';
+            }
+        }
+        if (pro_num == 7) {
+            cout << a[0];
+            REP(j,3){
+                if (bit & (1<<j)){
+                    cout << "-";
+                } else {
+                    cout << "+";
+                }
+                cout << a[j+1];
+            }
+            cout << "=7" << endl;
+            return 0;
+        }
+    }
 }
