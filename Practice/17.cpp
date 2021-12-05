3fd147a355577dbf0c6cee877dd066d87aca3e6f
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-12-05 16:33:58 +0900

    17

--- Practice/17.cpp
+++ Practice/17.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc085/tasks/abc085_c
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,14 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N,Y;cin >> N >> Y;
+    REP(i,N+1)REP(j,N+1){
+        ll left = Y - (i*(10000) + j*(5000));
+        if (left%1000 != 0)continue;
+        if (left >= 0 && i+j+(left/1000) == N){
+            cout << i << " " << j << " " << left/1000 << endl;
+            return 0;
+        }
+    }
+    cout << -1 << " " << -1 << " " << -1 << endl;
 }
