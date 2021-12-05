69192dcbd2d6102a60f786c882c0a71594fafd91
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-12-05 16:37:36 +0900

    18

--- Practice/18.cpp
+++ Practice/18.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc175/tasks/abc175_b
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,13 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N;cin >> N;
+    vector<int> L(N,-1);
+    REP(i,N)cin >> L[i];
+    int ans =0;
+    FOR(i,0,N)FOR(j,i+1,N)FOR(k,j+1,N){
+        if (L[i] == L[j] || L[j] == L[k] || L[k] == L[i])continue;
+        if (L[i]+L[j] > L[k] && L[j]+L[k] > L[i] && L[k]+L[i] > L[j])ans++;
+    }
+    cout << ans << endl;
 }
