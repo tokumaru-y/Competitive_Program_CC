e7e8d697fcd90b1f6a3d452cb0a9a3fb803dd931
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-12-05 16:22:02 +0900

    15

--- Practice/15.cpp
+++ Practice/15.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/arc004/tasks/arc004_1
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,13 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N;cin >> N;
+    vector<int> X(N,-1);vector<int> Y(N,-1);
+    REP(i,N) cin >> X[i] >> Y[i];
+    double ans =0.0;
+    REP(i,N)FOR(j,i+1,N){
+        int a = X[i], b = Y[i],c = X[j],d = Y[j];
+        ans = max(ans, sqrt(pow(a-c,2.0) + pow(b-d,2.0)));
+    }
+    cout << ans << endl;
 }
