b7efda3867749785c74077077965134f67fda15f
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-12-05 17:01:33 +0900

    19

--- Practice/19.cpp
+++ Practice/19.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/arc061/tasks/arc061_a
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,19 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    string S;cin >> S;
+    int size = S.size();
+    ll ans = 0;
+    for(int bit=0;bit<(1<<(size-1)); bit++){
+        ll progrece_num = S[0]-'0';
+        for(int i=0;i<size-1;i++){
+            if (bit & (1<<i)){
+                ans += progrece_num;
+                progrece_num=0;
+            }
+            progrece_num = progrece_num*10 + S[i+1] - '0';
+        }
+        ans += progrece_num;
+    }
+    cout << ans << endl;
 }
