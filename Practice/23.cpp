2f7a5d183d2ad0be7b5311cb49cc63b56f4bc52f
tokumaru_y <tokumaru.program.yuuki@gmail.com>
2021-12-05 18:45:38 +0900

    23

--- Practice/23.cpp
+++ Practice/23.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc002/tasks/abc002_4
 #include <bits/stdc++.h>
 #define FOR(i,a,b) for(int i=(a);i<(b);i++)
 #define REP(i,n) for(int i=0;i < (n);i++)
@@ -12,4 +13,29 @@ const int MOD = (int)1e9 + 7;
 vector<int> dx={1,0,-1,0};vector<int> dy={0,-1,0,1};
 
 signed main () {
+    int N,M;cin >> N >> M;
+    vector<vector<int>> edge(N,vector<int>(N,-1));
+    REP(i,M){
+        int x,y;
+        cin >> x >> y;
+        x--,y--;
+        edge[x][y] = 1;
+        edge[y][x] = 1;
+    }
+    int ans = -1;
+    for(int bit=0;bit<(1<<N);bit++){
+        vector<int>list;
+        for(int i=0;i<N;i++){
+            if(bit & (1<<i))list.push_back(i);
+        }
+        bool flag=true;
+        for(auto j: list){
+            for(auto k: list){
+                if (j==k)continue;
+                if (edge[j][k] != 1)flag=false;
+            }
+        }
+        if(flag)ans=max(ans, (int)list.size());
+    }
+    cout << ans << endl;
 }
